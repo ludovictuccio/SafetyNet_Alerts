b7849890ad4a163abae29d38262c69a025d64544
Ludovic Tuccio <ludovic.tuccio@hotmail.fr>
2020-06-03 18:10:41 +0200

    Spring boot configuration added
    pom.xml changed

---
+++ src/main/java/src/main/java/com/safetynet/alerts/SafetyNetAlertsApplication.java
@@ -0,0 +1,13 @@
+package src.main.java.com.safetynet.alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyNetAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyNetAlertsApplication.class, args);
+	}
+
+}
