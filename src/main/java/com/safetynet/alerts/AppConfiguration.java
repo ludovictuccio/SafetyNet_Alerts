729c794051c8d2017c51fc27dfe72b627ccc7c3d
Ludovic Tuccio <ludovic.tuccio@hotmail.fr>
2020-06-16 11:09:23 +0200

    Httptrace endpoint functional
    Added httptrace configuration, and maven-site plugin in pom.xml to generate reports.

---
+++ src/main/java/com/safetynet/alerts/AppConfiguration.java
@@ -0,0 +1,27 @@
+package com.safetynet.alerts;
+
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Application configuration.
+ * 
+ * @author Ludovic Tuccio
+ *
+ */
+@Configuration
+public class AppConfiguration {
+
+   /**
+    * Method used to configure httptrace endpoint with actuator.
+    * 
+    * @return new InMemoryHttpTraceRepository
+    */
+   @Bean
+   public HttpTraceRepository htttpTraceRepository() {
+      return new InMemoryHttpTraceRepository();
+   }
+
+}
